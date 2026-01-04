2343ec43af35301cb7c2cc4c7d97ec6dbfbdfc2b
Joseph Charles Dasilva Barbosa <josephbarbosa416@gmail.com>
2026-01-03 19:18:10 -0500

    defined training and output JSON schemas for LLM

--- schemas.py
+++ schemas.py
@@ -2,25 +2,6 @@
 from pydantic import BaseModel
 from typing import List, Dict
 
-class RecipeRequest(BaseModel):
-    ingredients: List[str]
-    restrictions: List[str]
-    preferences: List[str]
-
-class Nutrition(BaseModel):
-    calories: float
-    protein: float
-    fat: float
-    carbs: float
-
-class RecipeResponse(BaseModel):
-    title: str
-    ingredients: List[str]
-    estimated_time: float
-    instructions: List[str]
-    restrictions: List[str]
-    nutrition: Nutrition
-
 class IngredientInferenceRequest(BaseModel):
     ingredients: List[str]
 
@@ -29,4 +10,15 @@ class IngredientLabel(BaseModel):
     present: bool
 
 class IngredientInferenceResponse(BaseModel):
-    labels: dict[str, IngredientLabel]
+    labels: Dict[str, IngredientLabel]
+
+class LLMRequest(BaseModel):
+    ingredients_on_hand: List[str]
+    forbidden_ingredients: Dict[str, int]
+    preferences: str
+
+class LLMResponse(BaseModel):
+    title: str
+    ingredients: List[str]
+    directions: List[str]
+    
